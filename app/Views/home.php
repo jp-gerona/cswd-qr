1ddf0764803f93e6925c6ffa3702ca6c7d0a8e27
jp-gerona <jp-gerona@protonmail.com>
2026-06-22 09:16:09 +0800

    initial commit

---
+++ app/Views/home.php
@@ -0,0 +1,13 @@
+<?= $this->extend("layouts/base") ?>
+
+<?= $this->section("title") ?>
+    CSWD QR Code Generator
+<?= $this->endSection() ?>
+
+<?= $this->section("description") ?>
+    This serves as an application for generating QR codes.
+<?= $this->endSection() ?>
+
+<?= $this->section("content") ?>
+    <p>Hello World!</p>
+<?= $this->endSection() ?>
---
+++ app/Views/layouts/base.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?= $this->renderSection("title") ?></title>
+    <meta name="description" content="<?= $this->renderSection(
+        "description",
+    ) ?>" />
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+  </head>
+  <body>
+    <main>
+      <?= $this->renderSection("content") ?>
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <?= $this->renderSection("scripts") ?>
+  </body>
+</html>
