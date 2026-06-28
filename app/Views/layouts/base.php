3209d617860c4a72ed247f0e1d8ab18ca1082343
jp-gerona <jp-gerona@protonmail.com>
2026-06-28 08:21:54 +0800

    chore(deps): migrate remaining cdn to composer libs

--- app/Views/layouts/base.php
+++ app/Views/layouts/base.php
@@ -8,15 +8,15 @@
         "description",
     ) ?>" />
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
   </head>
   <body>
     <main>
       <?= $this->renderSection("content") ?>
     </main>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/jquery.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <?= $this->renderSection("scripts") ?>
   </body>
